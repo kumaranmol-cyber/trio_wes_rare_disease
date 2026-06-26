3bd62b1f71d31dd7b02ebc60b0a4cb61e19c93b9
kumaranmol-cyber <kumaranmol1507@gmail.com>
2026-06-26 17:37:17 +0530

    Add Trimmomatic preprocessing script

--- scripts/02_trimmomatic.sh
+++ scripts/02_trimmomatic.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+# ==========================================================
+# Step 02 - Adapter Trimming using Trimmomatic
+# ==========================================================
+
+echo "=========================================="
+echo "Running Trimmomatic..."
+echo "=========================================="
+
+mkdir -p ../results/trimmed
+
+for SAMPLE in HG002 HG003 HG004
+do
+    trimmomatic PE \
+    ../data/raw/${SAMPLE}/${SAMPLE}_R1.fastq.gz \
+    ../data/raw/${SAMPLE}/${SAMPLE}_R2.fastq.gz \
+    ../results/trimmed/${SAMPLE}_R1_paired.fastq.gz \
+    ../results/trimmed/${SAMPLE}_R1_unpaired.fastq.gz \
+    ../results/trimmed/${SAMPLE}_R2_paired.fastq.gz \
+    ../results/trimmed/${SAMPLE}_R2_unpaired.fastq.gz \
+    ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+    LEADING:20 \
+    TRAILING:20 \
+    SLIDINGWINDOW:4:20 \
+    MINLEN:50
+done
+
+echo "=========================================="
+echo "Trimming Completed Successfully!"
+echo "Output saved in results/trimmed/"
+echo "=========================================="
