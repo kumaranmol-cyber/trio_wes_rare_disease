9aece8d4a4b325d9cacfadcb61d89897437be5ee
kumaranmol-cyber <kumaranmol1507@gmail.com>
2026-06-26 17:38:47 +0530

    Add BWA-MEM alignment script

--- scripts/03_bwa_align.sh
+++ scripts/03_bwa_align.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+# ==========================================================
+# Step 03 - Read Alignment using BWA-MEM
+# ==========================================================
+
+echo "=========================================="
+echo "Starting BWA-MEM Alignment"
+echo "=========================================="
+
+mkdir -p ../results/aligned
+
+REFERENCE=../data/reference/hg38.fa
+
+for SAMPLE in HG002 HG003 HG004
+do
+    echo "Aligning ${SAMPLE}..."
+
+    bwa mem -t 8 \
+    $REFERENCE \
+    ../results/trimmed/${SAMPLE}_R1_paired.fastq.gz \
+    ../results/trimmed/${SAMPLE}_R2_paired.fastq.gz \
+    > ../results/aligned/${SAMPLE}.sam
+
+    echo "${SAMPLE} alignment completed."
+done
+
+echo "=========================================="
+echo "BWA Alignment Completed Successfully!"
+echo "SAM files saved in results/aligned/"
+echo "=========================================="
