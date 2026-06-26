2752a10d9cd40dd5b245eaa8606f60f355b3bd12
kumaranmol-cyber <kumaranmol1507@gmail.com>
2026-06-26 17:40:55 +0530

    Add GATK HaplotypeCaller script

--- scripts/05_gatk_haplotypecaller.sh
+++ scripts/05_gatk_haplotypecaller.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+# ==========================================================
+# Step 05 - Variant Calling using GATK HaplotypeCaller
+# ==========================================================
+
+echo "=========================================="
+echo "Running GATK HaplotypeCaller"
+echo "=========================================="
+
+mkdir -p ../results/variants
+
+REFERENCE=../data/reference/hg38.fa
+
+for SAMPLE in HG002 HG003 HG004
+do
+    echo "Calling variants for ${SAMPLE}..."
+
+    gatk HaplotypeCaller \
+        -R $REFERENCE \
+        -I ../results/aligned/${SAMPLE}_sorted.bam \
+        -O ../results/variants/${SAMPLE}.g.vcf.gz \
+        -ERC GVCF
+
+    echo "${SAMPLE} variant calling completed."
+done
+
+echo "=========================================="
+echo "Variant Calling Completed Successfully!"
+echo "Output saved in results/variants/"
+echo "=========================================="
